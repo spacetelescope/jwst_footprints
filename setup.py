d3dcaa047379f3b92b19801882a6c46556da61ea
Joseph Hunkeler <jhunkeler@gmail.com>
2017-06-02 09:29:37 -0400

    Initial commit of setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='jwst_footprints',
+    version='1.2.0',
+    author='Leonardo Lubeda',
+    author_email='lubeda@stsci.edu',
+    description='TODO',
+    url='https://github.com/spacetelescope/jwst_footprints',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires=[
+        'astropy',
+        'numpy',
+        'scipy',
+    ],
+
+    packages=find_packages(),
+    package_data={
+        '': [
+            'data/*',
+        ]
+    },
+    entry_points={
+        'console_scripts': [
+            'jwst_footprints=jwst_footprints:main',
+        ],
+    },
+)
