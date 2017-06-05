34d84835a51e3e5b859e740959062d14afbee0ab
Joseph Hunkeler <jhunkeler@gmail.com>
2017-06-05 16:29:47 -0400

    Move gui application to own package

--- jwst_footprints/gui/footprints.py
+++ jwst_footprints/gui/footprints.py
@@ -5,10 +5,10 @@ from __future__ import absolute_import, division, print_function
 import json
 import os
 
-from . import PKG_DATA_DIR, CONFIG_DIR, CONFIG_FILE
-from .footprints import footprints
-from .defaults import default_config
-from .plot_timeline import plottimeline
+from .. import PKG_DATA_DIR, CONFIG_DIR, CONFIG_FILE
+from ..footprints import footprints
+from ..defaults import default_config
+from ..plot_timeline import plottimeline
 
 try:
     from Tkinter import (Tk, Button, Entry, Label, OptionMenu, StringVar, \
